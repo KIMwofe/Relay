cc354de4e39cdab2ed784a4f2ec86bd2c8516ad9
kim <245902043@qq.com>
2020-02-07 00:37:59 +0800

    头像

--- src/store/index.js
+++ src/store/index.js
@@ -10,14 +10,14 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    uid: 968,
+    uid: 0,
     goodsId: 0
   },
   mutations: {
-    // setAuth(state, val) {
-    //   let { uid, goodsId } = val;
-    //   state.uid = uid;
-    //   state.goodsId = goodsId;
-    // }
+    setAuth(state, val) {
+      let { uid, goodsId } = val;
+      state.uid = uid;
+      state.goodsId = goodsId;
+    }
   }
 });
